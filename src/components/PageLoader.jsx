d8b4b33dd6b4a50e5e6d4ff7b88ba68a439d9d4e
Jonathan Soukaseume <soomie@Jonathans-MacBook-Pro.local>
2026-05-18 13:48:01 -0700

    style: add loading text to spinner

--- src/components/PageLoader.jsx
+++ src/components/PageLoader.jsx
@@ -61,6 +61,7 @@ function PageLoader({ children }) {
     <>
       {showSpinner && (
         <div className="loading-screen">
+          <h1>Loading...</h1>
           <div className="spinner" />
         </div>
       )}
